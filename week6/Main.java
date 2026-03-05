ef18afa7997121386cadc7bf749fb7747b070372
steven <shotta3xd@gmail.com>
2026-03-05 13:56:02 -0500

    added content

---
+++ week6/Main.java
@@ -0,0 +1,28 @@
+package week6;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Main {
+
+   
+
+    public static void main(String[] args) {
+        
+        // 1. Create a list of pirates with their names, roles, and bounties
+        
+        // 2. Use a stream to filter out pirates with a bounty of 100 or more
+        // whats a stream? its a way to process collections of data in a functional style
+        
+        // 3. Print the names and bounties of the filtered pirates
+        
+
+        // 4. Use an iterator to remove pirates with the role of "Apprentice"
+        
+        // 5. Print the remaining pirates in the crew
+        
+    }
+    
+}
